9ebd58bd596a5823008894549135552f56f8b28b
l3rady <l3rady@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-05-04 09:33:00 +0000

    git-svn-id: https://plugins.svn.wordpress.org/scraps/branches/0.1.1@539807 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/tinymce_langs.php
@@ -0,0 +1,11 @@
+<?php
+$strings = '
+    tinyMCE.addI18n(
+        "' . _WP_Editors::$mce_locale . '.scScraps",
+        {
+            dialog_title:"' . esc_js( __('Add a Scrap', 'scraps') )  . '",
+            button_title:"' . esc_js( __('Add a Scrap', 'scraps') )  . '"
+        }
+    );
+';
+?>
